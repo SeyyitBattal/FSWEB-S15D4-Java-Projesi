b0b3033c4e099cf9f98b12d2fec574b056d08749
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-10-05 13:53:26 +0300

    Palindrome with stack & queue done

--- src/com/workintech/StackChallenge/PalindromeSandQ.java
+++ src/com/workintech/StackChallenge/PalindromeSandQ.java
@@ -1,22 +1,27 @@
 package com.workintech.StackChallenge;
 
-import java.util.Stack;
+import java.util.*;
 
-public class PalindromeStack {
+public class PalindromeSandQ {
     public static boolean isPalindrome(String input) {
         if (input == null || input.isEmpty()) {
             return false;
         }
 
         String cleanedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (char c : cleanedInput.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
-        for (char c : cleanedInput.toCharArray()) {
-            if (c != stack.pop()) {
+
+        while (!stack.isEmpty() && !queue.isEmpty()) {
+            char stackChar = stack.pop();
+            char queueChar = queue.poll();
+
+            if (stackChar != queueChar) {
                 return false;
             }
         }
@@ -30,13 +35,10 @@ public class PalindromeStack {
                 "hello",
                 "Was it a car or a cat I saw ?"
         };
-
+        System.out.println("Palindrome (Stack & Queue)\n");
         for (String text : texts) {
             boolean isPalindrome = isPalindrome(text);
-            System.out.println("Palindrome (Just Stack)\n");
             System.out.println("\"" + text + "\" => " + (isPalindrome ? "palindrome" : "not palindrome"));
         }
-
     }
-
 }
